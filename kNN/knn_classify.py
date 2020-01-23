ac20f630ffe390f8fe88730600f876109fa38e4b
Saket Roy <saket.roy@hotmail.com>
2020-01-23 13:06:33 +0530

    Correctly calculate the std deviation of a dimension

--- kNN/knn_classify.py
+++ kNN/knn_classify.py
@@ -1,13 +1,24 @@
 from sys import argv
 import numpy as np
+import math
 
 script, pendigits_training, pendigits_test = argv
 
 training_dataset = open(f"{pendigits_training}.txt", 'r')
 
 matrix = []
-
 for line in training_dataset:
     matrix.append(line.split())
 
-print(matrix[0])
+np_arr = np.array(matrix).astype(np.float)
+np_arr.transpose()
+
+def getStd(dimension):
+    mean = np.mean(dimension)
+    sigma = 0
+    for elem in dimension:
+        sigma += abs((elem - mean)**2)
+    
+    std = math.sqrt(sigma/len(dimension))
+
+    return std
