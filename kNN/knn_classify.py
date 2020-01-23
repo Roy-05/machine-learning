4a6ad705b6e3b9f3a19dbec8bec4d6c54b761c6d
Saket Roy <saket.roy@hotmail.com>
2020-01-23 13:30:55 +0530

    normalize data points

--- kNN/knn_classify.py
+++ kNN/knn_classify.py
@@ -11,14 +11,26 @@ for line in training_dataset:
     matrix.append(line.split())
 
 np_arr = np.array(matrix).astype(np.float)
-np_arr.transpose()
+np_arr = np_arr.transpose()
 
 def getStd(dimension):
     mean = np.mean(dimension)
     sigma = 0
-    for elem in dimension:
-        sigma += abs((elem - mean)**2)
+    for datapoint in dimension:
+        sigma += abs((datapoint - mean)**2)
     
     std = math.sqrt(sigma/len(dimension))
 
     return std
+
+
+for i in range(0, len(np_arr)-1):
+    row = np_arr[i]
+    std = getStd(row)
+    mean = np.mean(row)
+    for j in range(0, len(row)):
+        row[j] = (row[j] - mean)/std
+
+np_arr = np_arr.transpose()
+
+print(np_arr[0])
