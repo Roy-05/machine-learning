6663ad53c74cde72988a6bdc76c566ae4fb1d547
Saket Roy <saket.roy@hotmail.com>
2020-01-30 16:06:23 +0530

    add tkinter to regressor file

--- Linear Regression/regressor.py
+++ Linear Regression/regressor.py
@@ -1,11 +1,12 @@
 from random import randint, random
+import tkinter
 
 x_points = []
 y_points = []
-w = randint(1, 10)
+w = randint(1, 5)
 b = randint(1, 10)
 datapoints = 20
-epochs = 50
+epochs = 2000
 learning_rate = 0.000001
 
 for i in range(datapoints):
@@ -27,15 +28,52 @@ def update_weight_and_bias(w, b):
     w_derive = 0
     b_derive = 0
     for i in range(datapoints):
-        w_derive += -2 * x_points[i] * (y_points[i] - (w * x_points[i] + b))
-        b_derive += -2 * (y_points[i] - (w * x_points[i] + b))
+        w_derive += x_points[i] * (y_points[i] - (w * x_points[i] + b))
+        b_derive += y_points[i] - (w * x_points[i] + b)
     
-    w -= (w_derive / datapoints) * learning_rate
-    b -= (b_derive / datapoints) * learning_rate
+    w += (w_derive / float(datapoints)) * learning_rate
+    b += (b_derive / float(datapoints)) * learning_rate
 
     return w,b
 
-for i in range(epochs):
-    w1,b1 = update_weight_and_bias(w1, b1)
-    print(w1, b1)
-    
+top = tkinter.Tk()
+canvas_width = 500
+canvas_height = 500
+
+canvas = tkinter.Canvas(top, height=canvas_width, width=canvas_height)
+
+start_x = min(x_points)
+start_y = start_x * w + b
+
+end_x = 100
+end_y = 100 * w + b
+
+canvas.create_line(start_x, 
+                canvas_height - start_y, 
+                end_x, 
+                canvas_height - end_y, fill = "#ff5500")
+
+def create_circle(x, y, r, canvasName): 
+    x0 = x - r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
+    return canvasName.create_oval(x0, y0, x1, y1)
+
+for i in range(datapoints):
+    create_circle(x_points[i], canvas_height - y_points[i], 3, canvas)
+
+# for i in range(epochs):
+#     w1, b1 = update_weight_and_bias(w1, b1)
+
+#     start_y = start_x * w1 + b1
+#     end_y = 100 * w1 + b1
+
+#     canvas.create_line(start_x, 
+#                     canvas_height - start_y, 
+#                     end_x, 
+#                     canvas_height -end_y)
+
+
+canvas.pack()
+top.mainloop()
--- canvas.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-top = tkinter.Tk()
-
-C = tkinter.Canvas(top, height=500, width=500)
-
-
-C.pack()
-top.mainloop()
