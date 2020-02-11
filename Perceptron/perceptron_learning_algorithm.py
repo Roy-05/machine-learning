2588ce46d82bae4fdd967b0ffee3e6636d15a99b
Saket Roy <saket.roy@hotmail.com>
2020-02-12 00:59:26 +0530

    Set up activation func and feedforward

--- Perceptron/perceptron_learning_algorithm.py
+++ Perceptron/perceptron_learning_algorithm.py
@@ -1,17 +1,35 @@
-import random
+from random import random, randint, uniform
 import math
 
 #Slope ranges from  PI/12 - 5PI/12
-m = random.uniform(math.pi/12, 5*math.pi/12)
-c = random.randint(-50,50)
+m = uniform(math.pi/12, 5*math.pi/12)
+c = randint(-50,50)
+learning_rate = 0.0001
 
 dataset = []
+weights = [random(), random(), random()] #Initialize with Bias
 
+print(weights)
+#Generatate Datapoints
 for i in range(20):
-    x = random.randint(50,950)
-    y = random.randint(50,950)
-    pt_class = 1 if y - m*x - c >= 0 else 0
+    x = randint(50,950)
+    y = randint(50,950)
+    pt_class = 1 if y - m*x - c >= 0 else -1
     dataset.append([x, y, pt_class])
+
+def activate(s):
+    return 1 if s >= 0 else -1
+
+def feedForward(row):
+    sigma = weights[0]
+    for weight,x in zip(weights[1:], row):
+        sigma += weight*x
     
+    return activate(sigma)
+
+for row in dataset:
+    e = feedForward(row)
+    print(f"Expect: {row[-1]} Actual: {e}")
+
 
 
