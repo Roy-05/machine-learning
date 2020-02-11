522f4aa00ee170fe34d174dc23a9c34ed5ce4083
Saket Roy <saket.roy@hotmail.com>
2020-02-12 00:29:44 +0530

    Create data set and classify points

--- Perceptron/perceptron_learning_algorithm.py
+++ Perceptron/perceptron_learning_algorithm.py
@@ -1,7 +1,17 @@
 import random
 import math
 
-#Slope ranges from  PI/12 - PI/3
-m = random.uniform(math.pi/12, math.pi/3)
-c = random.randint(-100,100)
+#Slope ranges from  PI/12 - 5PI/12
+m = random.uniform(math.pi/12, 5*math.pi/12)
+c = random.randint(-50,50)
+
+dataset = []
+
+for i in range(20):
+    x = random.randint(50,950)
+    y = random.randint(50,950)
+    pt_class = 1 if y - m*x - c >= 0 else 0
+    dataset.append([x, y, pt_class])
+    
+
 
