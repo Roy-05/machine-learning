0f04cde401f51fefcb9272b58f98d451ceda83ae
Saket Roy <saket.roy@hotmail.com>
2020-02-11 23:12:52 +0530

    Set up initial canvas

---
+++ Perceptron/canvas.py
@@ -0,0 +1,11 @@
+import perceptron_learning_algorithm as pla
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from random import random
+
+fig = plt.figure(num = "Perceptron Learning Algrotihm")
+
+ax = plt.axes(xlim = [0, 1000], ylim = [0, 1000])
+
+
+plt.show()
