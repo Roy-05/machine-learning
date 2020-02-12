ade2affb98498eb5cc0a51fee69dec57edf54db2
Saket Roy <saket.roy@hotmail.com>
2020-02-12 23:39:13 +0530

    Algorithm now functions correctly

--- Perceptron/perceptron_learning_algorithm.py
+++ Perceptron/perceptron_learning_algorithm.py
@@ -5,30 +5,64 @@ import math
 m = uniform(math.pi/12, 5*math.pi/12)
 c = randint(-50,50)
 learning_rate = 0.0001
+datapoints = 50
+epochs = 0
+points = []
 
 dataset = []
 weights = [random(), random(), random()] #Initialize with Bias
 
-print(weights)
 #Generatate Datapoints
-for i in range(20):
+for i in range(datapoints):
     x = randint(50,950)
     y = randint(50,950)
-    pt_class = 1.00 if y - m*x - c >= 0.00 else -1.00
+    pt_class = 1.0 if y - m*x - c >= 0.00 else -1.0
     dataset.append([1, x, y, pt_class])
 
 def activate(s):
-    return 1.00 if s >= 0.00 else -1.00
+    return 1.0 if s >= 0.0 else -1.0
 
 def feedForward(row):
-    sigma = weights[0]
+    sigma = 0
     for weight,x in zip(weights, row):
         sigma += weight*x
     
     return activate(sigma)
 
 def train(row):
-    output = feedForward(row)
-    for i in range(len(weights)):
+    output = feedForward(row[0:3])
+    for i in range(len(row)-1):
         weights[i] += learning_rate * (row[-1] - output) * row[i]
 
+def getCurrentPoints(points):
+    """
+    Derived from w1x1 + w2x2 + w3x3 = 0
+    """
+    
+    x0 = 0
+    y0 = - weights[0]/weights[2]
+    xn = 1000
+    yn = - (weights[1] * 1000 + weights[0])/ weights[2]
+
+    points.append([[x0,xn], [y0,yn]])
+
+while(True):
+    epochs += 1
+    converged = True
+    misclassify = 0
+
+    getCurrentPoints(points)
+
+    for row in dataset:
+        classify = feedForward(row[0:3])
+        train(row)
+    
+        print(f"Expect: {row[-1]:4} Actual: {classify:4}  {row[-1]==classify}")
+        if(row[-1] != classify):
+            converged = False
+            misclassify += 1
+
+    print(f"\nEpoch:{epochs} misclassified:{misclassify}\n")
+    if(converged):
+        break 
+   
