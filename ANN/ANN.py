3ee77c9bc0b7b2e45acd9820abd831bd021f2219
Saket Roy <saket.roy@hotmail.com>
2020-02-26 15:23:33 +0530

    Create dataset from training file

--- ANN/ANN.py
+++ ANN/ANN.py
@@ -1,10 +1,15 @@
 import os
+
+
 dataset = []
 filename = os.getcwd() + r'/ANN/data/optdigits-3.tra'
+
 
 def create_dataset(filename):
     f = open(filename, 'r')
     for line in f:
-        print(line)
+        line = line.rstrip('\n')
+        dataset.append([line])
+    print(dataset)
 
 create_dataset(filename)
