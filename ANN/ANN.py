dd728c2c18631250fb7e1a274f81deac4c388d89
Saket Roy <saket.roy@hotmail.com>
2020-02-26 15:45:32 +0530

    Initialize a nn with specific input, hidden, and output neurons

--- ANN/ANN.py
+++ ANN/ANN.py
@@ -1,7 +1,8 @@
 import os
+from random import random
 
-
-dataset = []
+neural_net = list()
+dataset = list()
 filename = os.getcwd() + r'/ANN/data/optdigits-3.tra'
 
 
@@ -10,6 +11,24 @@ def create_dataset(filename):
     for line in f:
         line = line.rstrip('\n')
         dataset.append([line])
-    print(dataset)
+    f.close()
 
+def initialize_neural_network(n_inputs, n_hidden, n_outputs):
+    """
+    n_inputs: number of neurons in input layer\n
+    n_hidden: number of neurons in hidden layer\n
+    n_outputs: number of neurons in output layer\n
+    """
+
+    # +1 to add the bias
+    hidden_layer = [{'w': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    neural_net.append(hidden_layer)
+
+    output_layer = [{'w':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    neural_net.append(output_layer)
+
+# Create our dataset from the file by invoking this function
 create_dataset(filename)
+
+# Initialize our neural net by invoking this function
+initialize_neural_network(64, 8, 4)
