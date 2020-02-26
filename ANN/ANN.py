6d17ed92494ff99b5be7cee6c1e168e3e08ee2a6
Saket Roy <saket.roy@hotmail.com>
2020-02-26 20:28:33 +0530

    backPropagation algorithm is functional

--- ANN/ANN.py
+++ ANN/ANN.py
@@ -2,16 +2,20 @@ import os
 from math import exp
 from random import random
 
-neural_net = list()
-dataset = list()
+
 filename = os.getcwd() + r'/ANN/data/optdigits-3.tra'
 
+neural_net, dataset, training_set, test_set = [],[],[],[]
+
+epochs = 100
+expected = [0,1,2,3]
 
 def create_dataset(filename):
     f = open(filename, 'r')
     for line in f:
-        line = line.rstrip('\n')
-        dataset.append([int(i) for i in line.split(',')])
+        line = line.rstrip('\n').split(',')
+        
+        dataset.append([(int(line[i])/16.0) if i != len(line) - 1 else int(line[i]) for i in range(len(line))])
     f.close()
 
 
@@ -57,21 +61,47 @@ def forwardPropagation(row):
         inputs = new_inputs
     return inputs
 
+# Calculate the derivative of a neuron output
+def sigmoid_prime(y):
+    return y * (1.0 - y)
 
 
+def backPropagation():
+    for i in range(len(neural_net)-1, -1, -1):
+        layer = neural_net[i]
+        errors = []
+        q = len(layer)
 
+        # Case: Output layer
+        if i == (len(neural_net) - 1):
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron["output"])
+        else:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in neural_net[i+1]:
+                    error += neuron['w'][j] * neuron['delta']
+                errors.append(error)
+        
+
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * activate(neuron['output'])
 
 
 # Create our dataset from the file by invoking this function
 create_dataset(filename)
 
 # Initialize our neural net by invoking this function
-initialize_neural_network(64, 8, 4)
-
-
+initialize_neural_network(64, 2, 4)
 
 #Test
-# row = dataset[0][:-1]
-# output = forwardPropagation(row)
+row = dataset[0][:-1]
+output = forwardPropagation(row)
 
-# print(output)
+# Backpropagate and update weights
+backPropagation()
+
+for layer in neural_net:
+    print(layer)
